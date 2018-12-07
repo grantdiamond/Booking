21b21608b06bca1b0d8c0c5235c3b3fb9d31cd11
Grant Diamond <grantadiamond@gmail.com>
2018-12-07 11:43:26 -0500

    feat(database): rebuilds offsets daily at midnight
    every night the database offsets get thrown off. these functions change the offset immediately every
    night at midnight, then begin rebuilding the database. this happens sequentially so that during the
    rebuilding process requests can be properly handled with either a -1 offset for entries that have
    not been rebuilt yet, a normal offset for entries that have been rebuilt, or a 200ms timeout for
    entries that are about to be rebuilt.

---
+++ database/rebuilders.js
@@ -0,0 +1,91 @@
+const Redis = require('ioredis');
+const redis = new Redis();
+const params = require('./index');
+const { checkNextID } = require('./databaseHelpers');
+const { key, bits, chunkSize, chunkBegin } = require('./constants');
+const Promise = require('bluebird')
+
+var rebuilding = false;
+var rebuilt = 1;
+var counter = 0;
+
+const rebuildRedis = function(total) {
+  if (rebuilt < total) {
+    if ((total - rebuilt) > chunkSize) {
+      rebuildChunk(rebuilt, chunkSize)
+      .then(response => {
+        rebuilt += chunkSize;
+        counter++
+        if (counter % 100 === 0) {
+          console.log('rebuilt ' + (rebuilt / total * 100).toFixed(2) + ' %')
+        }
+        return rebuildRedis(total);
+      })
+      .catch(err => {
+        return err
+      });
+    } else {
+      let remainder = total - rebuilt;
+      return rebuildChunk(rebuilt, remainder)
+      .then(response => {
+        rebuilt += remainder;
+        return rebuildRedis(total);
+      })
+      .catch(err => {
+        return err
+      });
+    }
+  } else {
+    rebuilding = false;
+    rebuilt = 1;
+    return 'Database rebuilt!';
+  }
+};
+
+var buildGetQueries = function(idx, sizeOfChunk) {
+  var args = [];
+  for (let i = idx; i < (idx + sizeOfChunk); ++i) {
+    var offset = i * bits - bits;
+    var queries = [
+      'get', 'u29', offset + 39,
+      'get', 'u30', offset + 68,
+      'get', 'u30', offset + 98]
+    args = args.concat(queries);
+  }
+  return args;
+}
+
+var buildSetQueries = function(idx, result) {
+  var queries = [];
+  for (let i = 0; i < result.length; i += 3) {
+    var offset = (idx + (i / 3)) * bits - bits;
+    var query = [
+      'set', 'u29', offset + 38, result[i],
+      'set', 'u30', offset + 67, result[i + 1],
+      'set', 'u30', offset + 97, result[i + 2]
+    ];
+    queries = queries.concat(query);
+  }
+  return queries
+}
+
+var rebuildChunk = function(begin, sizeOfChunk) {
+  var args = buildGetQueries(begin, sizeOfChunk);
+  return redis.bitfield(key, args)
+  .then(result => {
+    return buildSetQueries(begin, result)  
+  })
+  .then(queries => {
+    return redis.bitfield(key, queries)
+  })
+  .then(() => {
+    return `chunk rebuilt! begin ${begin} end ${begin + sizeOfChunk}`
+  })
+  .catch(err => {
+    return err;
+  });
+}
+
+exports.areWorking = rebuilding;
+exports.rebuilt = rebuilt;
+exports.rebuildRedis = rebuildRedis;
