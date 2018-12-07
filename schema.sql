694538ef67176b0eadaca63defa763b9b0332fe7
Grant Diamond <grantadiamond@gmail.com>
2018-12-07 16:31:31 -0500

    fix(schema): replaced all tabs with spaces

--- schema.sql
+++ schema.sql
@@ -7,25 +7,25 @@ USE booking;
 
 CREATE TABLE apartment (
 
-	id int NOT NULL AUTO_INCREMENT,
-	apartmentid int NOT NULL,
-	price int NOT NULL,
-    max int NOT NULL,
-    minStay int NOT NULL,
-    stars dec(4,2) NOT NULL,
-    numRatings int NOT NULL,
-	PRIMARY KEY (id)
+  id int NOT NULL AUTO_INCREMENT,
+  apartmentid int NOT NULL,
+  price int NOT NULL,
+  max int NOT NULL,
+  minStay int NOT NULL,
+  stars dec(4,2) NOT NULL,
+  numRatings int NOT NULL,
+  PRIMARY KEY (id)
 
 );
 
 CREATE TABLE dates (
 
-	id int NOT NULL AUTO_INCREMENT,
-	date varchar (20) NOT NULL,
-	apartment_id int NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (apartment_id)
-		REFERENCES apartment (id)
+  id int NOT NULL AUTO_INCREMENT,
+  date varchar (20) NOT NULL,
+  apartment_id int NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (apartment_id)
+  REFERENCES apartment (id)
 
 );
 
