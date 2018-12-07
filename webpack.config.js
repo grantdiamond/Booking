b91a68abcccf44596318e602507275a4afb36135
Grant Diamond <grantadiamond@gmail.com>
2018-12-07 18:45:05 -0500

    build(webpack bundling): new build script

--- webpack.config.js
+++ webpack.config.js
@@ -8,31 +8,33 @@ module.exports = {
     filename: 'bundle.js',
     path: DIST_DIR
   },
-  module : {
-    rules : [
+  module: {
+    rules: [
       {
-        test : /\.jsx?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',      
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015']
-       }
+        }
       },
       {
-        test: /\.css$/,  
-        include: /node_modules/,  
-        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /\.css$/,
+        include: /node_modules/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
-        test: /\.(png|jp(e*)g|svg)$/,  
-        use: [{
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [
+          {
             loader: 'url-loader',
-            options: { 
-                limit: 8000, // Convert images < 8kb to base64 strings
-                name: 'images/[hash]-[name].[ext]'
-            } 
-        }]
-    }
+            options: {
+              limit: 8000, // Convert images < 8kb to base64 strings
+              name: 'images/[hash]-[name].[ext]'
+            }
+          }
+        ]
+      }
     ]
   }
 };
